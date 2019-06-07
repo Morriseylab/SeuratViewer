dc98aa5d0f289454eaf4a689fd67c2248d905f44
bapoorva <bapoorva@mail.med.upenn.edu>
2019-06-07 13:50:29 -0400

    add colors to color palette

--- server.R
+++ server.R
@@ -34,7 +34,8 @@ source("functions.R")
 cpallette=c("#64B2CE", "#DA5724", "#74D944", "#CE50CA", "#C0717C", "#CBD588", "#5F7FC7",
             "#8B4484", "#D3D93E", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD",
             "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D",
-            "#8A7C64", "#599861","#000099","#FFCC66","#99CC33","#CC99CC","#666666", "#695F74")
+            "#8A7C64", "#599861","#000099","#FFCC66","#99CC33","#CC99CC","#666666", "#695F74","#0447F9",
+            "#89134F","#2CF7F0","#F72C35","#A5B617","#B05927","#B78ED8")
 
 #Specify user-ids and passwords
 auth=read.csv("data/authentication.csv")
